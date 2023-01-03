c911a795348895b3a7dd262378f42f98a6cfafce
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2023-01-03 19:58:30 -0300

    feat: create the generic feed project with react JS, and vite

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { Post } from './Post';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar'
+
+import './global.css';
+import styles from './App.module.css';
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="v 76 ae trueno"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo deleniti magni recusandae, veniam autem alias nesciunt inventore aspernatur ipsa, possimus, libero mollitia maiores doloremque ullam cumque beatae iste quibusdam adipisci?"
+          />
+
+          <Post
+            author="ae ! trueno"
+            content="Lorem ipsum dolusandae, veniam autem  nesciunt inventore aspernatur ipsa, possimus, libero mollitia maiores doloremque ullam cumque beatae iste quibusdam adipisci?"
+          />  
+        </main>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1568283661163-c90193fd13f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"/>
+
+      <div className={styles.profile}>
+        <string>V Silver</string>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
