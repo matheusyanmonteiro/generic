868450242458dd95b96e0e35b5c825fd943d28c1
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2023-01-04 21:16:22 -0300

    feat: finished sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine} from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -6,12 +7,15 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1568283661163-c90193fd13f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"/>
 
       <div className={styles.profile}>
-        <string>V Silver</string>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/49919267?v=4"/>
+
+        <strong>V Silver</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size="20"/>
           Editar seu perfil
         </a>
       </footer>
